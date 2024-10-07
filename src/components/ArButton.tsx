2210935ee0cd160348881e76a28f35a09cbfa2e6
Leandro Gomez <leandro.grotnig@gmail.com>
2024-10-07 14:37:47 -0300

    Adds button to request access to the camera

--- src/components/ArButton.tsx
+++ src/components/ArButton.tsx
@@ -7,9 +7,13 @@ interface Props {
 
 export const ArButton: React.FC<Props> = (props) => {
     const { label, children, square, onClick } = props;
-    const classes: string[] = ['h-14 bg-white flex-center rounded-md'];
+    const classes: string[] = [
+        'h-14 bg-white flex-center rounded-md hover:bg-gray-200',
+    ];
     if (square) {
-        classes.push('w-14');
+        classes.push('w-14 px-0');
+    } else {
+        classes.push('px-6');
     }
 
     return (
--- src/components/CameraView.tsx
+++ src/components/CameraView.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useRef, useState } from 'react';
+import { ArButton } from './ArButton';
 
 export const CameraView: React.FC = () => {
+    const [init, setInit] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<string>(null);
     const videoRef = useRef<HTMLVideoElement>(null);
 
-    useEffect(() => {
+    const onInit = () => {
+        setLoading(true);
         if (navigator.mediaDevices.getUserMedia) {
             navigator.mediaDevices
                 .getUserMedia({ video: true })
@@ -15,18 +19,34 @@ export const CameraView: React.FC = () => {
                 })
                 .catch((err: string) => {
                     setError(err);
+                })
+                .finally(() => {
+                    setLoading(false);
+                    setInit(true);
                 });
         }
-    });
+    };
 
     return (
-        <div className="fixed size-full flex-center">
+        <div
+            className={`fixed size-full flex-center ${
+                init ? 'z-auto' : 'z-50'
+            }`}
+            onClick={onInit}
+        >
             <video
                 ref={videoRef}
                 className="size-full border border-red-500 object-cover object-center"
                 autoPlay={true}
             />
             {error && <span>{error}</span>}
+            {!init && (
+                <div className="absolute inset-0 flex-center z-30">
+                    <ArButton
+                        label={loading ? 'Cargando...' : 'Toca para comenzar!'}
+                    />
+                </div>
+            )}
         </div>
     );
 };
